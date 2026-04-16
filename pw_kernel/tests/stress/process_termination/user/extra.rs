0d298f21173dc4126e0ea490da85b18c73628916
Erik Gilling <konkers@google.com>
2026-04-16 15:21:41 -0700

    pw_kernel: Add process termination stress test
    Change-Id: I56aeb5b40cd5b22dd3a5702901b76800f855f1aa
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/393557

---
+++ pw_kernel/tests/stress/process_termination/user/extra.rs
@@ -0,0 +1,33 @@
+// Copyright 2026 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#![no_std]
+#![no_main]
+
+use pw_log::info;
+use userspace::{entry, syscall};
+
+#[entry]
+fn main() -> ! {
+    info!("I am the extra process. Spinning...");
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let _ = syscall::debug_shutdown(Err(pw_status::Error::Internal.into()));
+    loop {}
+}
---
+++ pw_kernel/tests/stress/process_termination/user/main.rs
@@ -0,0 +1,72 @@
+// Copyright 2026 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#![no_std]
+#![no_main]
+
+use main_codegen::handle;
+use pw_log::info;
+use pw_status::{Result, StatusCode};
+use time::Clock;
+use userspace::{entry, syscall};
+
+fn do_test() -> Result<()> {
+    info!("🔄 [User Process Termination Stress] RUNNING");
+
+    let mut pass = 0;
+    let timeout_ticks = 5 * userspace::time::Clock::TICKS_PER_SEC;
+
+    loop {
+        info!("🔄 ├─ Pass {}", pass as u32);
+
+        let result = syscall::process_terminate(handle::EXTRA_PROCESS);
+        if result.is_err() {
+            pw_log::error!("Failed to terminate extra process");
+            return Err(pw_status::Error::Internal.into());
+        }
+
+        info!("🔄 ├─ Waiting", pass as u32);
+        let now = syscall::debug_clock_now();
+        let deadline = userspace::time::Instant::from_ticks(now.ticks() + timeout_ticks);
+        syscall::object_wait(handle::EXTRA_PROCESS, syscall::Signals::JOINABLE, deadline)?;
+
+        info!("🔄 ├─ Joining", pass as u32);
+        syscall::process_join(handle::EXTRA_PROCESS)?;
+
+        info!("🔄 ├─ Starting", pass as u32);
+        syscall::process_start(handle::EXTRA_PROCESS)?;
+
+        pass += 1;
+    }
+}
+
+#[entry]
+fn main_entry() -> ! {
+    let ret = do_test();
+
+    if ret.is_err() {
+        pw_log::error!("❌ FAILED");
+        pw_log::error!("❌ status code: {}", ret.status_code() as u32);
+    }
+
+    let _ = syscall::debug_shutdown(ret);
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    pw_log::error!("❌ PANIC");
+    let _ = syscall::debug_shutdown(Err(pw_status::Error::Internal.into()));
+    loop {}
+}
